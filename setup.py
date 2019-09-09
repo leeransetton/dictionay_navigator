62fb0005c8ea2a0f5d75d8592ce4643b569c8929
Leeran Setton <leerans@optimalq.com>
2019-09-09 13:28:40 +0300

    README

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as reqsFile:
     requirements = reqsFile.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='dictionary_navigator',
       version='1.0.0',
       description='Dictionary Navigator With Strings',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/leeransetton/dictionay_navigator',
       author='Leeran Setton',
       author_email='leeransetton@gmail.com',
