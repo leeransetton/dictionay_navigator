f964bff36840928dcb357de218668b7455b4bac6
Leeran Setton <leerans@optimalq.com>
2019-09-09 13:32:50 +0300

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='dictionary_navigator',
-      version='1.0.0',
+      version='1.0.1',
       description='Dictionary Navigator With Strings',
       long_description=long_description,
       long_description_content_type="text/markdown",
